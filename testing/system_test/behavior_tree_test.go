125df684d2df5e474cfea689c3a2fddb76977624
ksks2012 <st890751999@gmail.com>
2025-02-19 17:51:19 +0800

    test(behavior_tree_test)

---
+++ testing/system_test/behavior_tree_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"testing"
+
+	bt "github.com/logos/ecs/systems/behavior/behavior_tree"
+)
+
+func TestBehaviorTree_SearchForResources(t *testing.T) {
+	bb := bt.NewBlackboard()
+	bb.Set("stamina", 30) // Set NPC stamina above threshold
+
+	behaviorTree := &bt.Selector{
+		Children: []bt.Node{
+			&bt.RestIfTired{},
+			&bt.Sequence{
+				Children: []bt.Node{
+					&bt.SearchForResources{},
+					&bt.MoveToTarget{},
+				},
+			},
+		},
+	}
+
+	result := behaviorTree.Execute(bb)
+	if result != bt.SUCCESS {
+		t.Errorf("Expected SUCCESS, got %v", result)
+	}
+}
+
+func TestBehaviorTree_MoveToTarget(t *testing.T) {
+	bb := bt.NewBlackboard()
+	bb.Set("stamina", 30)       // Set NPC stamina above threshold
+	bb.Set("targetFound", true) // Set target found
+
+	behaviorTree := &bt.Selector{
+		Children: []bt.Node{
+			&bt.RestIfTired{},
+			&bt.Sequence{
+				Children: []bt.Node{
+					&bt.SearchForResources{},
+					&bt.MoveToTarget{},
+				},
+			},
+		},
+	}
+
+	result := behaviorTree.Execute(bb)
+	if result != bt.SUCCESS {
+		t.Errorf("Expected SUCCESS, got %v", result)
+	}
+}
