9fb4094c97bc42eec2ab8f7e9e2b6fca0bcdc761
ksks2012 <st890751999@gmail.com>
2025-03-17 04:46:35 +0800

    feat(unit): Enhance Inanimate struct with UnitTypeID, Character, and Practice attributes

--- ecs/entities/unit/inanimate.go
+++ ecs/entities/unit/inanimate.go
@@ -3,25 +3,34 @@ package unit
 import (
 	"fmt"
 
+	"github.com/logos/ecs/components"
 	"github.com/logos/ecs/components/attributes"
 )
 
 type Inanimate struct {
-	ID          int64                         `json:"id"`
-	Name        string                        `json:"name"`
-	Description string                        `json:"description"`
-	LocationID  int64                         `json:"location_id"`
-	WuXing      attributes.WuXingAttributes   `json:"wuxing"`
-	Diagram     attributes.DiagramsAttributes `json:"diagram"`
+	ID          int64                          `json:"id"`
+	Name        string                         `json:"name"`
+	UnitTypeID  components.UnitType            `json:"unit_type_id"`
+	Character   attributes.CharacterAttributes `json:"character"` // For bunos of unit
+	Practice    attributes.PracticeAttributes  `json:"practice"`
+	Description string                         `json:"description"`
+	LocationID  int64                          `json:"location_id"`
+	WuXing      attributes.WuXingAttributes    `json:"wuxing"`
+	Diagram     attributes.DiagramsAttributes  `json:"diagram"`
 }
 
 // NewInanimate creates and initializes a new inanimate object
-func NewInanimate(id int64, name, description string, locationID int64) Inanimate {
+func NewInanimate(id int64, name string, unitTypeID components.UnitType, character attributes.CharacterAttributes, practice attributes.PracticeAttributes, description string, locationID int64, wuXing attributes.WuXingAttributes, diagram attributes.DiagramsAttributes) Inanimate {
 	return Inanimate{
 		ID:          id,
 		Name:        name,
+		UnitTypeID:  unitTypeID,
+		Character:   character,
+		Practice:    practice,
 		Description: description,
 		LocationID:  locationID,
+		WuXing:      wuXing,
+		Diagram:     diagram,
 	}
 }
 
